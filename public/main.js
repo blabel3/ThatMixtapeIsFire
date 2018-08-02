d93154efa7eaf02bf942b1d9a032422c0623c6bf
Blabel <blakecabel@gmail.com>
2018-08-02 18:13:08 -0500

    Added a target to drop the mixtape into

--- public/main.js
+++ public/main.js
@@ -1,6 +1,13 @@
 $( function() {
   $( "#cassette" ).draggable({
-    axis: "y",
-    disabled: false
+    //axis: "y",
+    disabled: false,
+    cursor: "move",
+    start: function( event, ui ) {
+
+    },
+    stop: function( event, ui ) {
+
+    }
   });
 } );
