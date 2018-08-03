4a8d307e4a3db6363022937343d6d56648057021
Blabel <blakecabel@gmail.com>
2018-08-02 19:04:10 -0500

    Added animations

--- public/main.js
+++ public/main.js
@@ -1,13 +1,28 @@
 $( function() {
+  $( "#heat" ).hide();
+
   $( "#cassette" ).draggable({
     //axis: "y",
     disabled: false,
     cursor: "move",
     start: function( event, ui ) {
-
+      $( this ).removeClass("glow");
     },
     stop: function( event, ui ) {
 
     }
   });
+
+  $( "#target" ).droppable({
+    drop: function( event, ui ) {
+      $( "#cassette" ).draggable( "option", "disabled", true );
+      $( document.body ).css("background", "url('assets/fire.jpg') repeat");
+      $( "#heat" ).show();
+      $( this ).remove();
+      $( "#cassette" ).addClass("party");
+      $( "#boombox" ).addClass("party");
+      $( "#homie" ).text("That was some HEAT my guy!!");
+    }
+  });
+
 } );
